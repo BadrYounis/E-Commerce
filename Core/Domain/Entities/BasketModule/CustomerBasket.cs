790bd59e35ebaf9a7f9198a7f89112e4b9bd2bb8
Badr <badryounis14@gmail.com>
2026-03-08 01:39:35 +0200

    Add Basket Entities

---
+++ Core/Domain/Entities/BasketModule/CustomerBasket.cs
@@ -0,0 +1,6 @@
+﻿namespace Domain.Entities.BasketModule;
+public class CustomerBasket
+{
+    public string Id { get; set; } = string.Empty;
+    public ICollection<BasketItem> BasketItems { get; set; } = [];
+}
